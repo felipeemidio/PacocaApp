b2ad17a1ce2d6c2cb5debdbba207ee01e1683eac
felipeemidio <lipe.emidio@gmail.com>
2021-06-13 16:36:37 -0300

    Add field to create products

--- lib/pages/items_list_page.dart
+++ lib/pages/items_list_page.dart
@@ -1,7 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:pacocaApp/consts/mock_data.dart';
+import 'package:pacocaApp/models/product.dart';
 
-class ItemsListPage extends StatelessWidget {
+class ItemsListPage extends StatefulWidget {
+  @override
+  _ItemsListPageState createState() => _ItemsListPageState();
+}
+
+class _ItemsListPageState extends State<ItemsListPage> {
+  final productFieldController = TextEditingController();
+
+  @override
+  void dispose() {
+    productFieldController.dispose();
+    super.dispose();
+  }
+
+  void _submit() {
+    setState(() {
+      int biggestId = 0;
+      mockProductsList.forEach((element) {
+        if(element.id > biggestId) {
+          biggestId = element.id;
+        }
+      });
+      final newProduct = Product(biggestId, productFieldController.text);
+      mockProductsList.add(newProduct);
+      productFieldController.clear();
+    });
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -12,11 +41,32 @@ class ItemsListPage extends StatelessWidget {
         ),
         centerTitle: true,
       ),
-      body: ListView.builder(
-        itemCount: mockProductsList.length,
-        itemBuilder: (_, index) => ListTile(
-          title: Text(mockProductsList[index].name),
-        ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 4),
+            child: TextField(
+              controller: productFieldController,
+              onSubmitted: (_) => _submit(),
+              decoration: InputDecoration(
+                hintText: 'Novo produto',
+                suffixIcon: IconButton(
+                  visualDensity: VisualDensity.compact,
+                  icon: Icon(Icons.add),
+                  onPressed: _submit,
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: mockProductsList.length,
+              itemBuilder: (_, index) => ListTile(
+                title: Text(mockProductsList[index].name),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
